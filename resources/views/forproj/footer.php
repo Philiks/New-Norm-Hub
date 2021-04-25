0d6b38d5c2e5210c9f64b3f4bc6aeb425ec9eb7f
MichaelCerro <82153261+miivkyo08@users.noreply.github.com>
2021-04-25 15:59:56 +0800

    Add files via upload

---
+++ resources/views/forproj/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<footer>
+    <div class="container-fluid" style="margin: 32px 0px 0px; background-color: #3380FF; padding: 20px; border-color: #3380FF; ; bottom: 0; width: 100%;  position: fixed;">
+        <div class="footer" style="background-color: #3380FF;">
+           <h4><strong><center>For more information visit and contact us in:</center></strong></h4>
+           <center>
+           <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
+           <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
+           <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
+           <p>Copyright © 2021 BSCS-3A </p>
+           </center>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
