54f60408f93266df97886945c66ada412f9f2c69
Philiks <fjanopol@gmail.com>
2021-04-28 18:48:15 +0800

    Make profile_id nullable

--- database/migrations/2021_04_18_234100_create_users_table.php
+++ database/migrations/2021_04_18_234100_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignUuid('profile_id')->constrained('photos');
+            $table->foreignUuid('profile_id')->nullable()->constrained('photos');
             $table->rememberToken();
             $table->timestamps();
         });
