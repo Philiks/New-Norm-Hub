876d9a2c90ad2146a0ec6faaa19daa6d994b984c
Philiks <fjanopol@gmail.com>
2021-04-28 18:53:08 +0800

    Update breeze components

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-black pb-6">
+    <div class="w-3/5 mt-6 px-6 py-4 bg-gradient-to-br from-orange shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
